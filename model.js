f7a8e1efd724e2d166eab06432b814ec12f9ae69
Billy F <willfira717@gmail.com>
2020-12-23 16:42:48 -0500

    added SRC folder and connected .js files to index.html

--- model.js
+++
@@ -1,7 +0,0 @@
-class Driver {
-    constructor(attributes) {
-        // whitelisted attributes get passed in as if I wrote this.id & this.name,  stores them, and returns the object with the proper attributes  Ex: ==> new Driver({id: 4, name: "Niki Lauda"})
-        let whitelist = ["id", "name"]
-        whitelist.forEach(attr => this[attr] = attributes[attr])
-    }
-}
