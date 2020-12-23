10ec11eedbb9724f418a16d722f9c95b025737d5
Billy F <willfira717@gmail.com>
2020-12-23 16:16:13 -0500

    clarified notes in model.js

--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 class Driver {
     constructor(attributes) {
-        // whitelisted attributes get passed in as if I wrote this.id, this.name and stores them. Ex: ==> new Driver({id: 4, name: "Niki Lauda"})
+        // whitelisted attributes get passed in as if I wrote this.id & this.name,  stores them, and returns the object with the proper attributes  Ex: ==> new Driver({id: 4, name: "Niki Lauda"})
         let whitelist = ["id", "name"]
         whitelist.forEach(attr => this[attr] = attributes[attr])
     }
