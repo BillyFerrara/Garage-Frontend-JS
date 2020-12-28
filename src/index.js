24369fd0056cf251797e877522fb88be67c4d412
Billy F <willfira717@gmail.com>
2020-12-28 14:47:09 -0500

    building out functions

--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@
 const carsURL = "http://localhost:3000/cars"
 const driversURL = "http://localhost:3000/drivers"
 
-const carList = document.getElementById('car-list')
+const driverForm = document.getElementById('driver-form')
+
+const driverInput = document.getElementById('driver-input')
+
+const driverList = document.getElementById('driver-list')
+
+driverForm.addEventListener('submit', submitDriver)
+
+function submitDriver() {
+    event.preventDefault()
+    console.log('sDriver')
+}
+
+function renderDriver() {
+    event.preventDefault();
+   // console.log('submited')
+   const li = document.createElement('li');
+   const p = document.createElement('p');
+   p.innerHTML = driverInput.value
+   li.append(p)
+   driverList.appendChild(li)
+   driverForm.reset();
+}
+
